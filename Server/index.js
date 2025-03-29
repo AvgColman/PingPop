f227e29765d542f961937213ec66b821a329a686
Hayden <hnyquist@umich.edu>
2025-03-28 23:51:12 -0400

    Added more chat features
    "(username) is typing..." functionality
    "(username) has connected/disconnected" functionality

--- Server/index.js
+++ Server/index.js
@@ -1,9 +1,22 @@
-import { createServer } from "http"
+import express from 'express'
 import { Server } from "socket.io"
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-const httpServer = createServer()
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
-const io = new Server(httpServer, {
+const PORT = process.env.PORT || 3500
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, "public")))
+
+const expressServer = app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
+})
+
+const io = new Server(expressServer, {
     cors: {
         origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:5500","http://127.0.0.1:5500"]
     }
@@ -12,10 +25,24 @@ const io = new Server(httpServer, {
 io.on('connection', socket => {
     console.log(`User ${socket.id} connected`)
 
+    //send connection message to user
+    socket.emit('message', "Welcome to PingPop Chat!")
+    //send connection message to all users
+    socket.broadcast.emit('message', `User ${socket.id.substring(0,5)} connected`)
+
     socket.on('message', data => {
         console.log(data)
         io.emit('message', `${socket.id.substring(0,5)}: ${data}`)
     })
+
+    //disconnection messages
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('message', `User ${socket.id.substring(0,5)} disconnected`)
+    })
+
+    //user is typing message
+    socket.on('activity', (name) => {
+        socket.broadcast.emit('activity', name)
+    })
 })
 
-httpServer.listen(3500, () => console.log('listening on port 3500'))
